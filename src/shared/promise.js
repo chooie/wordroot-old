90e2640ddfe23a8af5a032fdf880ee7f2451401f
Charlie Hebert <charlie.hebert92@gmail.com>
2016-03-03 01:22:44 +0000

    Add Q library for promises

---
+++ src/shared/promise.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 Charles Hebert. All rights reserved. See LICENSE.txt 
+// for details.
+(function() {
+  "use strict";
+
+  var Q = require("../../vendor/q-1.4.1");
+
+  module.exports = Q;
+
+}());
