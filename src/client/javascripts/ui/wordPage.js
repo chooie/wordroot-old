61190deed74c331ed008b3581ab29c6d9d9a9ee2
Charlie Hebert <charlie.hebert92@gmail.com>
2016-03-12 16:40:41 +0000

    Tidy up wordPage

--- src/client/javascripts/ui/wordPage.js
+++ src/client/javascripts/ui/wordPage.js
@@ -4,24 +4,20 @@
   "use strict";
 
   var util = require("./util");
-  var constants = require("../constants");
+  var css = require("../constants").cssClasses;
   var revealer = require("./revealer");
 
   function addTitle(container, rootParts, roots) {
-    var containerClass = constants.cssClasses.container;
-    var titleClass = "title";
-    var options = { classes: [ containerClass, titleClass ] };
+    var options = { classes: [ css.container, "title" ] };
     var title = util.addElement(container, "div", options);
     var rootPartElems = addRootParts(title, rootParts);
     var rootElems = addRoots(title, roots);
     revealer.initialise(rootPartElems, rootElems);
-  }
+    return title;
+    }
 
   function addRootParts(container, rootParts) {
-    var rootPartsClass = constants.cssClasses.rootParts;
-    var classes = [ rootPartsClass ];
-    var options = { classes: classes };
-    var div = util.addElement(container, "div", options);
+    var div = util.addElement(container, "div", { classes: [ css.rootParts ] });
     var rootPartElems = [];
     rootParts.forEach(function(rootPart, i) {
       rootPartElems.push(createRootPart(div, rootPart, i));
@@ -29,56 +25,59 @@
     return rootPartElems;
 
     function createRootPart(container, rootPart, index) {
-      var COLORS = ["blue", "green", "red"];
-      var rootClass = constants.cssClasses.rootPart;
-      var options = { id: "root-part-" + index, classes: [ rootClass ]};
-      var rootPartElem = util.addElement(container, "p", options);
-      rootPartElem.style.color = COLORS[index % COLORS.length];
-      rootPartElem.innerHTML = rootPart;
-      return rootPartElem;
+      var elem = util.addElement(container, "p", { classes: [ css.rootPart ]});
+      setColorBasedOnIndex(elem, index);
+      elem.innerHTML = rootPart;
+      return elem;
+
+      function setColorBasedOnIndex(elem, i) {
+        var COLORS = ["blue", "green", "red"];
+        elem.style.color = COLORS[i % COLORS.length];
+      }
     }
   }
 
   function addRoots(container, roots) {
-    var rootClass = "roots";
-    var classes = [ rootClass ];
-    var div = util.addElement(container, "div", { classes: classes });
+    var div = util.addElement(container, "div", { classes: [ "roots" ] });
     var rootElems = [];
-    roots.forEach(function(root, index) {
-      rootElems.push(createRootInfoElem(div, root, index));
+    roots.forEach(function(root) {
+      rootElems.push(addRootInfoElem(div, root));
     });
     return rootElems;
 
-    function createRootInfoElem(container, root, index) {
-      var rootElemClass = "root-info";
-      var classes = [ rootElemClass ];
-      var options = { id: rootElemClass + "-" + index, classes: classes  };
-      var rootElem = util.addElement(container, "div", options);
-      var word = util.addElement(rootElem, "div",
-        { classes: [ "root-word" ] });
-      word.innerHTML = root.word;
-      var meaning = util.addElement(rootElem, "div",
-        { classes: [ "root-meaning" ] });
-      meaning.innerHTML = root.meaning;
-      var language = util.addElement(rootElem, "div",
-        { classes: [ "root-language" ] });
-      language.innerHTML = root.language;
+    function addRootInfoElem(container, root) {
+      var rootElemOptions = { classes: [ "root-info" ]  };
+      var rootElem = util.addElement(container, "div", rootElemOptions);
+      addWord(rootElem);
+      addMeaning(rootElem);
+      addLanguage(rootElem);
       return rootElem;
+
+      function addWord(container) {
+        var word = util.addElement(container, "div",
+          { classes: [ "root-word" ] });
+        word.innerHTML = root.word;
+      }
+
+      function addLanguage(container) {
+        var language = util.addElement(container, "div",
+          { classes: [ "root-language" ] });
+        language.innerHTML = root.language;
+      }
+
+      function addMeaning(container) {
+        var meaning = util.addElement(container, "div",
+          { classes: ["root-meaning"] });
+        meaning.innerHTML = root.meaning;
+      }
     }
   }
 
   function addMeaning(container, meaning) {
-    var containerClass = constants.cssClasses.container;
-    var meaningClass = constants.cssClasses.meaning;
-    var classes = [ containerClass, meaningClass ];
+    var classes = [ css.container, css.meaning ];
     var div = util.addElement(container, "div", { classes: classes });
-    var meaningElem = createMeaning(div);
-
-    function createMeaning(container) {
-      var p = util.addElement(container, "p");
-      p.innerHTML = meaning;
-    }
-    return meaningElem;
+    div.innerHTML = meaning;
+    return div;
   }
 
   module.exports = {
