4d17a997ea1a85200bbd6562e581b10f8ef2887a
Charlie Hebert <charlie.hebert92@gmail.com>
2016-04-05 23:54:16 +0100

    Attempt at querying database -- TODO fix this and tidy up

--- src/server/routes.js
+++ src/server/routes.js
@@ -52,10 +52,67 @@
       word = connection.escape(word);
 
       connection.query("SELECT * FROM words " +
-        "WHERE words.word = " + word, function (err, rows) {
+        "WHERE word = " + word, function(err, rows) {
         if (err) throw err;
 
-        res.send(rows[0]);
+        var wordObj = rows[0];
+
+        var wordId = connection.escape(wordObj.id);
+
+        connection.query("SELECT id, part FROM word_parts " +
+          "WHERE word_id = " + wordId + " " +
+          "ORDER BY position_index", function(err, wordParts) {
+
+          var parts = [];
+          var rootIds = [];
+          var roots = [];
+          wordParts.forEach(function(wordPart, i) {
+            parts.push(wordPart.part);
+
+            connection.query("SELECT root_id FROM word_parts_roots " +
+              "WHERE word_part_id = " + wordPart.id, function(err, rootId) {
+              rootIds.push(rootId[0] && rootId[0].root_id);
+
+              if (i === wordParts.length - 1) {
+                rootIds.forEach(function(rootId, j) {
+                  connection.query("SELECT * FROM roots " +
+                    "WHERE id = " + rootId, function(err, root) {
+                    roots.push(root && root[0]);
+
+                    if (j === rootIds.length - 1) {
+                      var languages = [];
+                      roots.forEach(function(root, i) {
+                        connection.query("SELECT name FROM languages " +
+                          "WHERE id = " +
+                          root.language_id, function(err, lang) {
+                          languages.push(lang[0].name);
+
+                          if (i === roots.length - 1) {
+                            roots.forEach(function(root, i) {
+                              roots[i] = {
+                                word: root.word,
+                                meaning: root.meaning,
+                                language: languages[i]
+                              };
+                            });
+                            wordObj.roots = [];
+                            parts.forEach(function(part, i) {
+                              wordObj.roots[i] = {
+                                part: parts[i],
+                                root: roots[i]
+                              };
+                            });
+                            res.send(wordObj);
+                          }
+                        });
+                      });
+                    }
+                  });
+                });
+              }
+            });
+          });
+        });
       });
     });
   };
