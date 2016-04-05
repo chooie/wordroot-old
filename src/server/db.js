f1933c3cd066beb5751273a240dc861b39f291f7
Charlie Hebert <charlie.hebert92@gmail.com>
2016-04-05 17:53:46 +0100

    Attempt to use mysql db -- todo: install mysql driver

---
+++ src/server/db.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 Charles Hebert. All rights reserved. See LICENSE.txt 
+// for details.
+(function() {
+  "use strict";
+
+  var mysql = require("mysql");
+
+  module.exports = mysql;
+
+}());
--- src/server/server.js
+++ src/server/server.js
@@ -9,10 +9,31 @@
   var express = require("express");
   var app = express();
 
+  var db = require("./db");
+
+  var connection = db.createConnection({
+    host: 'localhost',
+    user: 'root',
+  });
+
 	var server;
 
 	exports.start = function(portNumber, dirToServe, callback) {
     app.use(express.static(dirToServe));
+
+    app.get("/hello", function(req, res) {
+      connection.connect();
+
+      connection.query('SELECT 1 + 1 AS solution', function(err, rows
+                                                            /*fields*/) {
+        if (err) throw err;
+        console.log('The solution is: ', rows[0].solution);
+        res.send(rows[0].solution);
+      });
+
+      connection.end();
+    });
+
     server = app.listen(portNumber, callback);
 	};
 
